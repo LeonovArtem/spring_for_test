539a4cada4555ae142e4f7b4945bca7de8323675
Artem Leonov <arttemleonov@gmail.com>
2021-05-19 18:28:04 +0300

    Сериализация: сериализация массива спсособ_1

--- src/main/java/com/example/demo/shild_book/Resources/Serialize/Main.java
+++ src/main/java/com/example/demo/shild_book/Resources/Serialize/Main.java
@@ -1,6 +1,7 @@
 package com.example.demo.shild_book.Resources.Serialize;
 
 import java.io.*;
+import java.util.Arrays;
 
 public class Main {
     public static final String INPUT_FILE = "files/input_file.bin";
@@ -15,11 +16,13 @@ public class Main {
                 FileInputStream file = new FileInputStream(INPUT_FILE);
                 ObjectInputStream objectInputStream = new ObjectInputStream(file);
         ) {
-            Person person_1 = (Person) objectInputStream.readObject();
-            Person person_2 = (Person) objectInputStream.readObject();
+            int personCount = objectInputStream.readInt();
+            Person[] people = new Person[personCount];
 
-            System.out.println(person_1);
-            System.out.println(person_2);
+            for (int i = 0; i < personCount; i++) {
+                people[i] = (Person) objectInputStream.readObject();
+            }
+            System.out.println(Arrays.toString(people));
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
@@ -28,15 +31,15 @@ public class Main {
     private static void writeObjects() {
         Person person_1 = new Person(1, "Artem");
         Person person_2 = new Person(2, "Bob");
-
-        try {
-            FileOutputStream fileOutputStream = new FileOutputStream(INPUT_FILE);
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
-
-            objectOutputStream.writeObject(person_1);
-            objectOutputStream.writeObject(person_2);
-
-            objectOutputStream.close();
+        Person[] peoples = {person_1, person_2};
+        try (
+                FileOutputStream fileOutputStream = new FileOutputStream(INPUT_FILE);
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+        ) {
+            objectOutputStream.writeInt(peoples.length);
+            for (Person person : peoples) {
+                objectOutputStream.writeObject(person);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
